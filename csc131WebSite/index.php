91645f4c11a02569ecd754a4dad892c0a5747a5e
Alec Luna <alecluna@csus.edu>
2017-03-12 14:56:42 -0700

    Add files via upload

---
+++ csc131WebSite/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+
+  <link href="csc131Style.css" rel="stylesheet"> 
+  <title>CSC 131 Project</title>
+</head>
+
+
+<body>
+
+  <div class="wrapper">
+
+    <div class="header">
+
+      <div id="logo">
+
+          <!--img link in css -->
+
+        </div>
+
+        <div id="teamname">
+
+          <!--link our team name here i CSS -->
+
+          </div>
+    </div>
+
+    <div class="content">
+
+
+      <!-- this is where we put the main content, update, delete, php scripts, all of it lol -->
+       
+      </div>
+
+
+    </div>
+
+</body>
+
+</html>
